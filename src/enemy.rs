19bbc8b3a2a7a8c3db1e6c9ca6eade1eb9ad6f45
Zumstein Dominik <Dominik.Zumstein@gmx.net>
2025-04-05 01:19:57 +0200

    feat: impl enemy pushing

--- src/enemy.rs
+++ src/enemy.rs
@@ -4,7 +4,7 @@ use bevy::{prelude::*, time::common_conditions::on_timer};
 
 use rand::Rng;
 
-use crate::{AppSet, player::Player};
+use crate::{AppSet, movement::MovementController, player::Player};
 
 pub struct EnemyPlugin;
 impl Plugin for EnemyPlugin {
@@ -15,7 +15,15 @@ impl Plugin for EnemyPlugin {
                 .run_if(on_timer(Duration::from_millis(500)))
                 .in_set(AppSet::Update),
         );
-        app.add_systems(Update, enemy_movement);
+        app.add_systems(
+            Update,
+            (
+                enemy_movement,
+                enemy_collision_detection,
+                enemy_push_detection,
+            ),
+        )
+        .add_observer(enemy_pushing);
     }
 }
 
@@ -28,6 +36,15 @@ struct Speed(f32);
 
 #[derive(Component)]
 pub struct Enemy;
+
+#[derive(Component)]
+pub struct Health(f32);
+
+#[derive(Event)]
+pub struct PlayerEnemyCollision(pub Entity);
+
+#[derive(Event)]
+pub struct PlayerPushingEvent(pub Entity);
 
 fn spawn_enemy(
     mut commands: Commands,
@@ -52,12 +69,12 @@ fn spawn_enemy(
         },
         Transform::from_xyz(enemy_pos_x, enemy_pos_y, 0.),
         Enemy,
+        Health(10.),
         Speed(50.),
     ));
 
     Ok(())
 }
-
 fn enemy_movement(
     enemy_query: Query<(&mut Transform, &Speed), With<Enemy>>,
     player_query: Query<&Transform, (With<Player>, Without<Enemy>)>,
@@ -100,5 +117,61 @@ fn enemy_movement(
         let movement = (direction + separation_force).normalize() * (speed.0 * time.delta_secs());
         transform.translation += movement;
     }
+    Ok(())
+}
+
+fn enemy_collision_detection(
+    enemy_query: Query<(&mut Transform, Entity), With<Enemy>>,
+    player_query: Query<&mut Transform, (With<Player>, Without<Enemy>)>,
+    mut commands: Commands,
+) -> Result {
+    let player_pos = player_query.single()?;
+
+    for (&enemy_pos, enemy) in &enemy_query {
+        let distance_to_player = enemy_pos.translation.distance(player_pos.translation);
+
+        if distance_to_player <= SEPARATION_RADIUS {
+            commands.trigger(PlayerEnemyCollision(enemy));
+        }
+    }
+    Ok(())
+}
+
+fn enemy_push_detection(
+    enemy_query: Query<(&mut Transform, Entity), With<Enemy>>,
+    player_query: Query<&mut Transform, (With<Player>, Without<Enemy>)>,
+    mut commands: Commands,
+) -> Result {
+    let player_pos = player_query.single()?;
+
+    for (&enemy_pos, enemy) in &enemy_query {
+        let distance_to_player = enemy_pos.translation.distance(player_pos.translation);
+
+        if distance_to_player <= SEPARATION_RADIUS - 5.0 {
+            commands.trigger(PlayerPushingEvent(enemy));
+        }
+    }
+    Ok(())
+}
+
+fn enemy_pushing(
+    trigger: Trigger<PlayerPushingEvent>,
+    movement_query: Query<&MovementController, With<Player>>,
+    mut enemy_query: Query<(&mut Transform, Entity), (With<Enemy>, Without<Player>)>,
+    time: Res<Time>,
+) -> Result {
+    let push_entity = trigger.event().0;
+
+    let movement_controller = movement_query.single()?;
+    let velocity = movement_controller.max_speed * movement_controller.intent;
+
+    // let player_pos = movement_controller.1.translation;
+
+    for (mut enemy_pos, enemy_entity) in &mut enemy_query {
+        if enemy_entity == push_entity {
+            enemy_pos.translation += velocity.extend(0.0) * time.delta_secs();
+        }
+    }
+
     Ok(())
 }
