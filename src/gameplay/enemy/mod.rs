eebd20e85e144642f6c04c7d7920d7b9c4158560
DaAlbrecht <dadidi42@gmail.com>
2025-09-06 20:25:01 +0200

    feat: spawn walkers

--- src/gameplay/enemy/mod.rs
+++ src/gameplay/enemy/mod.rs
@@ -1,17 +1,16 @@
-use std::{f32::consts::PI, time::Duration};
+use std::f32::consts::PI;
 
 use bevy_enhanced_input::action::Action;
 
-use bevy::{prelude::*, time::common_conditions::on_timer};
+use bevy::prelude::*;
 use bevy_rand::{global::GlobalEntropy, prelude::WyRand};
 use rand::Rng;
 
 use crate::{
-    AppSystems,
     gameplay::{
         Health,
         player::{Direction, Move, PlayerHitEvent},
-        spells::{CastSpell, Despawn, Root, Spell},
+        spells::{CastSpell, Despawn, Spell},
     },
     screens::Screen,
 };
@@ -20,18 +19,14 @@ use super::player::Player;
 
 use super::spells::{Knockback, PlayerProjectile};
 
+mod walker;
+
 pub(crate) fn plugin(app: &mut App) {
-    app.add_systems(
-        Update,
-        spawn_enemy
-            .run_if(on_timer(Duration::from_millis(2000)))
-            .run_if(in_state(Screen::Gameplay))
-            .in_set(AppSystems::Update),
-    );
+    app.add_plugins(walker::plugin);
+
     app.add_systems(
         Update,
         (
-            enemy_movement,
             enemy_colliding_detection,
             enemy_stop_colliding_detection,
             enemy_push_detection,
@@ -125,55 +120,6 @@ fn spawn_enemy(
         DamageCooldown(Timer::from_seconds(0.5, TimerMode::Repeating)),
     ));
 
-    Ok(())
-}
-
-fn enemy_movement(
-    enemy_query: Query<(&mut Transform, &Speed, &Knockback, Option<&Root>), With<Enemy>>,
-    player_query: Query<&Transform, (With<Player>, Without<Enemy>)>,
-    time: Res<Time>,
-) -> Result {
-    let player_transform = player_query.single()?;
-
-    let enemy_positions = enemy_query
-        .iter()
-        .map(|t| t.0.translation)
-        .collect::<Vec<Vec3>>();
-
-    for (mut transform, speed, knockback, rooted) in enemy_query {
-        if knockback.0 > 1.0 || rooted.is_some() {
-            //skip movement if enemy gets knockedback or is rooted
-            continue;
-        }
-        let direction = (player_transform.translation - transform.translation).normalize();
-
-        // Separation force calculation for enemies
-        let mut separation_force = Vec3::ZERO;
-        for &other_pos in &enemy_positions {
-            // skip ourselves
-            if other_pos == transform.translation {
-                continue;
-            }
-            // Check if the distance between enemy `A` and all other enemies is less than the
-            // `SEPARATION_RADIUS`. If so, push enemy `A` away from the other enemy to maintain spacing.
-            let distance = transform.translation.distance(other_pos);
-            if distance < SEPARATION_RADIUS {
-                let push_dir = (transform.translation - other_pos).normalize();
-                let push_strength = (SEPARATION_RADIUS - distance) / SEPARATION_RADIUS;
-                separation_force += push_dir * push_strength * SEPARATION_FORCE;
-            }
-        }
-        // Separation force calculation for the player
-        let distance_to_player = transform.translation.distance(player_transform.translation);
-        if distance_to_player < SEPARATION_RADIUS {
-            let push_dir = (transform.translation - player_transform.translation).normalize();
-            let push_strength = (SEPARATION_RADIUS - distance_to_player) / SEPARATION_RADIUS;
-            separation_force += push_dir * push_strength * SEPARATION_FORCE;
-        }
-
-        let movement = (direction + separation_force).normalize() * (speed.0 * time.delta_secs());
-        transform.translation += movement;
-    }
     Ok(())
 }
 
---
+++ src/gameplay/enemy/walker.rs
@@ -0,0 +1,106 @@
+use std::{f32::consts::PI, time::Duration};
+
+use bevy::{prelude::*, time::common_conditions::on_timer};
+use bevy_rand::{global::GlobalEntropy, prelude::WyRand};
+use rand::Rng;
+
+use crate::{
+    AppSystems,
+    gameplay::{
+        enemy::{DamageCooldown, Enemy, SEPARATION_FORCE, SEPARATION_RADIUS, SPAWN_RADIUS, Speed},
+        player::Player,
+        spells::{Knockback, Root},
+    },
+    screens::Screen,
+};
+
+pub(crate) fn plugin(app: &mut App) {
+    app.add_systems(
+        Update,
+        spawn_walker
+            .run_if(on_timer(Duration::from_millis(2000)))
+            .run_if(in_state(Screen::Gameplay))
+            .in_set(AppSystems::Update),
+    );
+
+    app.add_systems(Update, (walker_movement).run_if(in_state(Screen::Gameplay)));
+}
+
+fn spawn_walker(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    player_query: Query<&Transform, With<Player>>,
+    mut rng: GlobalEntropy<WyRand>,
+) -> Result {
+    let player_pos = player_query.single()?;
+
+    let random_angle: f32 = rng.random_range(0.0..(2. * PI));
+    let random_radius: f32 = rng.random_range(0.0..10.);
+    let offset_x = (SPAWN_RADIUS + random_radius) * f32::sin(random_angle);
+    let offset_y = (SPAWN_RADIUS + random_radius) * f32::cos(random_angle);
+
+    let enemy_pos_x = player_pos.translation.x + offset_x;
+    let enemy_pos_y = player_pos.translation.y + offset_y;
+
+    commands.spawn((
+        Name::new("Default Enemy"),
+        Enemy,
+        Sprite {
+            image: asset_server.load("enemies/Walker.png"),
+            ..default()
+        },
+        Transform::from_xyz(enemy_pos_x, enemy_pos_y, 0.),
+        DamageCooldown(Timer::from_seconds(0.5, TimerMode::Repeating)),
+    ));
+
+    Ok(())
+}
+
+fn walker_movement(
+    enemy_query: Query<(&mut Transform, &Speed, &Knockback, Option<&Root>), With<Enemy>>,
+    player_query: Query<&Transform, (With<Player>, Without<Enemy>)>,
+    time: Res<Time>,
+) -> Result {
+    let player_transform = player_query.single()?;
+
+    let enemy_positions = enemy_query
+        .iter()
+        .map(|t| t.0.translation)
+        .collect::<Vec<Vec3>>();
+
+    for (mut transform, speed, knockback, rooted) in enemy_query {
+        if knockback.0 > 1.0 || rooted.is_some() {
+            //skip movement if enemy gets knockedback or is rooted
+            continue;
+        }
+        let direction = (player_transform.translation - transform.translation).normalize();
+
+        // Separation force calculation for enemies
+        let mut separation_force = Vec3::ZERO;
+        for &other_pos in &enemy_positions {
+            // skip ourselves
+            if other_pos == transform.translation {
+                continue;
+            }
+            // Check if the distance between enemy `A` and all other enemies is less than the
+            // `SEPARATION_RADIUS`. If so, push enemy `A` away from the other enemy to maintain spacing.
+            let distance = transform.translation.distance(other_pos);
+            if distance < SEPARATION_RADIUS {
+                let push_dir = (transform.translation - other_pos).normalize();
+                let push_strength = (SEPARATION_RADIUS - distance) / SEPARATION_RADIUS;
+                separation_force += push_dir * push_strength * SEPARATION_FORCE;
+            }
+        }
+        // Separation force calculation for the player
+        let distance_to_player = transform.translation.distance(player_transform.translation);
+        if distance_to_player < SEPARATION_RADIUS {
+            let push_dir = (transform.translation - player_transform.translation).normalize();
+            let push_strength = (SEPARATION_RADIUS - distance_to_player) / SEPARATION_RADIUS;
+            separation_force += push_dir * push_strength * SEPARATION_FORCE;
+        }
+
+        let movement = (direction + separation_force).normalize() * (speed.0 * time.delta_secs());
+        transform.translation += movement;
+    }
+    Ok(())
+}
