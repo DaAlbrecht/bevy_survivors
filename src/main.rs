999267aae069124ed8c7b8c52ba8412910c869d9
DaAlbrecht <dadidi42@gmail.com>
2025-11-23 14:53:50 +0100

    feat: start game muted with `cargo run -- --mute`

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 use bevy::prelude::*;
+use bevy_seedling::prelude::{MainBus, Volume, VolumeNode};
 
 fn main() -> AppExit {
-    App::new().add_plugins(bevy_survivors::plugin).run()
+    let mute = std::env::args().any(|arg| arg == "--mute");
+
+    App::new()
+        .add_plugins(bevy_survivors::plugin)
+        .add_systems(PostStartup, (mute_global_audio).run_if(move || mute))
+        .run()
+}
+
+fn mute_global_audio(mut master: Single<&mut VolumeNode, With<MainBus>>) {
+    master.volume = Volume::Linear(0.);
 }
