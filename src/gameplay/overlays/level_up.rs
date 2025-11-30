e63cd1b2ebbee0ced3426f87e0c8fd1097e7b2cc
dominika <dominik.albrecht@intersim.ch>
2025-11-30 13:39:49 +0100

    feat: new layout for level up screen

--- src/gameplay/overlays/level_up.rs
+++ src/gameplay/overlays/level_up.rs
@@ -1,6 +1,6 @@
 //! The level up menu.
 
-use bevy::prelude::*;
+use bevy::{color::palettes::basic, prelude::*, text::FontSmoothing};
 use bevy_rand::{global::GlobalRng, prelude::WyRand};
 use rand::Rng;
 
@@ -21,6 +21,7 @@ fn spawn_level_up_menu(
     mut rng: Single<&mut WyRand, With<GlobalRng>>,
 ) {
     let border_image = asset_server.load("kenny/panel-border-011.png");
+    let font: Handle<Font> = asset_server.load("ui/compass.ttf");
 
     commands
         .spawn((
@@ -34,7 +35,7 @@ fn spawn_level_up_menu(
                     Node {
                         position_type: PositionType::Relative,
                         width: Val::Percent(100.0),
-                        height: Val::Percent(30.0),
+                        height: Val::Percent(100.0),
                         column_gap: Val::Percent(10.0),
                         padding: UiRect::all(Val::Px(50.0)),
                         ..default()
@@ -58,7 +59,7 @@ fn spawn_level_up_menu(
                         };
                         parent
                             .spawn((
-                                item_choice_widget(border_image.clone(), spell_image),
+                                item_choice_widget(border_image.clone(), spell_image, &font),
                                 SpellType::ALL[spell_index],
                             ))
                             .observe(upgrade);
@@ -67,14 +68,21 @@ fn spawn_level_up_menu(
         });
 }
 
-fn item_choice_widget(border_image: Handle<Image>, spell_image: Handle<Image>) -> impl Bundle {
+fn item_choice_widget(
+    border_image: Handle<Image>,
+    spell_image: Handle<Image>,
+    font: &Handle<Font>,
+) -> impl Bundle {
     (
         Node {
             width: Val::Percent(100.0),
             height: Val::Percent(100.0),
-            align_items: AlignItems::Center,
             align_self: AlignSelf::Center,
-            justify_content: JustifyContent::Center,
+            flex_direction: FlexDirection::Column,
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::SpaceBetween,
+            row_gap: Val::Px(40.0),
+            padding: UiRect::all(Val::Px(40.0)),
             ..default()
         },
         ImageNode {
@@ -87,15 +95,68 @@ fn item_choice_widget(border_image: Handle<Image>, spell_image: Handle<Image>) -
             }),
             ..default()
         },
+        //Dark semi-transparent
+        BackgroundColor(Color::srgba(0.1, 0.1, 0.1, 0.6)),
         Button,
-        Children::spawn(Spawn((
-            Node {
-                width: Val::Percent(80.),
-                height: Val::Percent(80.),
-                ..Default::default()
+        Children::spawn((
+            Spawn((
+                Node {
+                    width: Val::Px(128.),
+                    height: Val::Px(128.),
+                    ..Default::default()
+                },
+                ImageNode::new(spell_image),
+            )),
+            Spawn(item_desc(font)),
+            Spawn(item_txt(font)),
+        )),
+    )
+}
+
+fn item_desc(font: &Handle<Font>) -> impl Bundle {
+    (
+        Node {
+            width: Val::Px(100.),
+            height: Val::Percent(100.),
+            flex_grow: 1.0,
+            ..Default::default()
+        },
+        Children::spawn((Spawn((
+            Text::new("Damage Increase"),
+            TextFont {
+                font: font.clone(),
+                font_size: 24.0,
+                font_smoothing: FontSmoothing::None,
+                ..default()
             },
-            ImageNode::new(spell_image),
-        ))),
+            TextLayout::new_with_justify(Justify::Center),
+            TextColor(Color::WHITE),
+        )),)),
+    )
+}
+
+fn item_txt(font: &Handle<Font>) -> impl Bundle {
+    //TODO: Get this from spell rarity proc
+    let colors = [basic::WHITE, basic::GREEN, basic::BLUE, basic::RED];
+    let rng = &mut rand::rng();
+    let color = colors[rng.random_range(0..colors.len())];
+    (
+        Node {
+            width: Val::Px(100.),
+            height: Val::Percent(100.),
+            ..Default::default()
+        },
+        Children::spawn((Spawn((
+            Text::new("Upgrade"),
+            TextFont {
+                font: font.clone(),
+                font_size: 32.0,
+                font_smoothing: FontSmoothing::None,
+                ..default()
+            },
+            TextLayout::new_with_justify(Justify::Center),
+            TextColor(color.into()),
+        )),)),
     )
 }
 
